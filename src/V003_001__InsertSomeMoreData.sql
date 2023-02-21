6e6c15efbc7dd5b03a273b1024203a77094134e9
cbsmith1 <smithcb1@verizon.net>
2023-02-21 12:18:03 -0500

    Initial checking

--- src/V003_001__InsertSomeMoreData.sql
+++ src/V003_001__InsertSomeMoreData.sql
@@ -1,3 +1,4 @@
 INSERT INTO Inventory VALUES (5, 'avacado', 160)
 INSERT INTO Inventory VALUES (6, 'pears', 162)
 INSERT INTO Inventory VALUES (7, 'watermelon', 164)
+INSERT INTO Inventory VALUES (8, 'watermelon', 166)
